2f67b00e14bd663369ea90333113a97f97499172
Mishajain1110 <117731761+Mishajain1110@users.noreply.github.com>
2024-01-12 18:16:42 +0530

    Map integration

---
+++ Road_Geometry_Software_Dev/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RoadGeometrySoftwareDevConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Road_Geometry_Software_Dev'
