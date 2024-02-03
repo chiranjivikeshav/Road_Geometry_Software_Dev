e3d5ae975f379c33750d6dd862648630245c30a6
chiranjivi <chiranjivikeshav1@gmail.com>
2024-02-03 22:27:23 +0530

    ajax request sent

--- roadgeometry/urls.py
+++ roadgeometry/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from roadgeometry import views
+from .views import save_coordinates
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('api/save-coordinates/', save_coordinates, name='save_coordinates'),
 ]
+
--- roadgeometry/views.py
+++ roadgeometry/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render,redirect
-from . import urls
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 def home(request):
     return render(request,'home.html')
+
+@csrf_exempt
+def save_coordinates(request):
+    if request.method == 'POST':
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        print(latitude)
+        print(longitude)
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
